4a3126e437045c951fbef014acbe824bb37b36b1
Matías Barrios <matias.barriosn@gmail.com>
2020-08-24 18:45:45 -0400

    sample created

--- sts/data-generator/datagenerator.py
+++ sts/data-generator/datagenerator.py
@@ -1,14 +1,30 @@
 from luckyseven import Luckyseven
+import random
+
 
 # Parameters
 b = 1
-n = 8
-mu = 7192
+n = 10
+mu = 6922
 p = 10000
 i = 5877
-j = 200
-
+j = 80
 l7 = Luckyseven()
 
-# Print result
-print(l7.prng(b, n, mu, i, j, p))
+
+# randomNumber = int(l7.prng(b, n, mu, i, j, p))
+# # Print result
+# print(randomNumber)
+
+# ToBinary
+# print(format(randomNumber, 'b').zfill(256))
+
+sampleSize = 10**6
+
+for sample in range(0, sampleSize):
+    print(sample)
+    mu = random.randint(0, 10**5)
+    randomNumber = int(l7.prng(b, n, mu, i, j, p))
+    binaryNumber = format(randomNumber, 'b').zfill(256)[:256]
+    with open("data.luckyseven", "a") as datafile:
+        datafile.write(f'{binaryNumber}\n')
